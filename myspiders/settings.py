23807b916a8c48b34825898f59fe3bdda2deaa7f
yi.zhang <yi.zhang@gh-ca.com>
2017-08-25 14:57:11 +0800

    添加redis

--- myspiders/settings.py
+++ myspiders/settings.py
@@ -18,7 +18,7 @@ NEWSPIDER_MODULE = 'myspiders.spiders'
 # USER_AGENT = 'myspiders (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
